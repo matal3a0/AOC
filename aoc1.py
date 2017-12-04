6024ee5ac233e2e0374c2785d3f9269ee47cff21
Johan <jbjorn@toymachine.lantm.intra.local>
2017-12-04 13:57:08 +0100

    Day 1, step 1

---
+++ aoc1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import sys
+
+line = sys.argv[1]
+strlen = len(sys.argv[1])
+sum = 0
+
+for i in range(0, int(strlen)):
+    a = line[i]
+    if i == strlen-1:
+        b = line[0] 
+    else:
+        b = line[i+1]
+
+    if a == b:
+        sum += int(a)
+
+print sum
+
