8293570fed68e4057b0c40076ead6c09b89069d0
Johan <jbjorn@toymachine.lantm.intra.local>
2017-12-04 23:48:30 +0100

    Day 3, step 2

---
+++ aoc3_2.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+import sys,math
+
+address = int(sys.argv[1])
+
+# Find square that will fit all numbers 1^2, 3^2, 5^2
+sqr_count = 1
+sqr = 1
+while (sqr < address):
+    sqr = math.pow(sqr_count, 2)
+    sqr_count += 2
+# Move counter back one step
+sqr_count -= 2
+size = int(math.ceil(sqr_count))  
+
+# Init 2D-array with zeros
+memory = [[0 for x in range(size)] for y in range(size)]
+
+# Location of port (1)
+port = int(sqr_count/2) 
+
+# Populate array
+a = 2 # start with two
+posx = port # start at port
+posy = port
+direction = 'e' # start going east
+memory[posy][posx] = 1 # insert 1 at port
+
+# Loop
+while True:
+    if direction == 'e':
+        posx += 1
+        if memory[posy+1][posx] == 0:
+            direction = 'n'
+    elif direction == 'n':
+        posy += 1
+        if memory[posy][posx-1] == 0:
+            direction = 'w'
+    elif direction == 'w':
+        posx -= 1
+        if memory[posy-1][posx] == 0:
+            direction = 's'
+    elif direction == 's':
+        posy -= 1
+        if memory[posy][posx+1] == 0:
+            direction = 'e'
+
+    # Value to insert
+    a = memory[posy][posx+1] + memory[posy][posx-1] + memory[posy+1][posx] + memory[posy-1][posx] + memory[posy+1][posx+1] + memory[posy+1][posx-1] + memory[posy-1][posx-1] + memory[posy-1][posx+1]
+    memory[posy][posx] = a
+    # If larger than address, we have the answer
+    if (a > address):
+        print "Answer: ", a
+        break
